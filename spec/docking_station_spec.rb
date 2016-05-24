4c802bc2cbd509e078d815054b31797ac8fcfb93
mtaner <mervetaner88@gmail.com>
2016-05-24 15:51:59 +0100

    Linked DockingStation class to Bike class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,15 @@
-require 'borisbikes'
+require 'docking_station'
 
 describe DockingStation do
   docking_station = DockingStation.new
+  bike = docking_station.release_bike
   # subject {docking_station}
     # it { is_expected.to respond_to(:release_bike) }
   it "should respond to release bike" do
     expect(docking_station).to respond_to(:release_bike)
   end
-end
+
+   it "should expect the bike to be working" do
+    expect(bike).to respond_to(:working?)
+  end
+ end
