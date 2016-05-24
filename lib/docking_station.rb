83e5800cf040031e82949b5ac054b0855ef0b0c2
Van Le <singularity@vannio.co.uk>
2016-05-24 17:01:57 +0100

    Added test to match user story #2

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,8 +2,18 @@ require_relative 'bike'
 
 class DockingStation
 
+  attr_reader :bike
+
   def release_bike
     Bike.new
+  end
+
+  def dock bike
+    @bike = bike
+  end
+
+  def bike
+    @bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,15 +1,24 @@
 require 'docking_station'
 
 describe DockingStation do
-  docking_station = DockingStation.new
-  bike = docking_station.release_bike
-  # subject {docking_station}
-    # it { is_expected.to respond_to(:release_bike) }
+  ds = DockingStation.new
+  bike = ds.release_bike
+
   it "should respond to release bike" do
-    expect(docking_station).to respond_to(:release_bike)
+    expect(subject).to respond_to(:release_bike)
   end
 
-   it "should expect the bike to be working" do
+  it "should expect the bike to be working" do
     expect(bike).to respond_to(:working?)
   end
+
+  it "should respond to docking one bike" do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
+
+  it "should show a bike that has been docked" do
+    docked_bike = ds.dock(bike)
+    expect(ds.bike).to eq docked_bike
+  end
+
  end
