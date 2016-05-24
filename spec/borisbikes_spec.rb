3d07d11dda91c23f5e2e08aa1b0f5ba72015b709
a-mellor <alexander.mellor@gmail.com>
2016-05-24 13:56:35 +0100

    Updated tests & added release_bike method

--- spec/borisbikes_spec.rb
+++ spec/borisbikes_spec.rb
@@ -1,5 +1,10 @@
 require 'borisbikes'
 
 describe DockingStation do
-
+  docking_station = DockingStation.new
+  # subject {docking_station}
+    # it { is_expected.to respond_to(:release_bike) }
+  it "should respond to release bike" do
+    expect(docking_station).to respond_to(:release_bike)
+  end
 end
