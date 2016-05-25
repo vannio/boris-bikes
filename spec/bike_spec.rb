992f79d2da3fefb86afea5f848dc81cd840957ec
kpuwal <kpuwal@gmail.com>
2016-05-25 15:22:02 +0100

    ...

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,7 @@ require 'bike'
 
 describe Bike do
   it { is_expected.to respond_to :working?}
+  it 'is working' do
+    expect(Bike.new.working?).to eq true 
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,8 @@ require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to (:release_bike) }
-  it 'releasea a bike' do
-    expects(Bike.new.release_bike).to_not be_nil
+
+  it 'release a bike' do
+    expect(subject.release_bike).to_not be_nil
   end
 end
