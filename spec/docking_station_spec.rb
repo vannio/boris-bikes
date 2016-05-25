4a4ee300af95c3cc158c396d4b06d879b7d6b750
Marco Araujo <MA@Marcos-MacBook-Air.local>
2016-05-25 14:14:51 +0100

    update day2

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,9 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to (:release_bike) }
+  it 'releasea a bike' do
+    expects(Bike.new.release_bike).to_not be_nil
+  end
 end
