4800db7cfad1a0746668c730be5025f5d4da9975
Cameron Epstein <cameron.a.epstein@gmail.com>
2016-05-25 13:53:59 +0100

    Boris Bikes: Ch11 Complete

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,7 +1,15 @@
 require_relative 'bike'
 
 class DockingStation
-	def release_bike
-		Bike.new
-	end
+
+  attr_reader :bike
+
+  def release_bike
+  	Bike.new
+  end
+  def dock_bike(bike)
+  	#docked? == true
+	@bike
+  end
+
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -3,11 +3,16 @@
 require 'DockingStation'
 
 describe DockingStation do
-	it "responds to release_bike" do
-		expect(subject).to respond_to :release_bike
-	end
-	it "releases the working bike" do
-		bike = subject.release_bike
-		expect(bike).to be_working
-		end 
+
+  it "responds to release_bike" do
+    expect(subject).to respond_to :release_bike
+end
+
+  it "releases the working bike" do
+	bike = subject.release_bike
+	expect(bike).to be_working
+end
+  it "docks the bike" do
+    expect(subject).to respond_to :dock_bike
+  end
 end
