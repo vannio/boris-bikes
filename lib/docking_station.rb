970a94d5112ff18085f9e267229d5036157d0f3d
Van Le <singularity@vannio.co.uk>
2016-05-26 12:08:33 +0100

    Added private methods full? and empty? on docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,18 +1,30 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bikes
+
   def initialize
-      @bikes= []
+    @bikes= []
   end
 
   def release_bike
-    raise "sorry, no bikes" unless @bikes.empty?
+    raise "sorry, no bikes" unless empty?
     @bikes.pop
   end
 
   def docking_bike(bike)
-    raise "sorry, dock is full" if @bikes.count >= 20
+    raise "sorry, dock is full" if full?
     @bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.size == 20 ? true : false
+  end
+
+  def empty?
+    @bikes.size == 0 ? true : false
   end
 
 end
