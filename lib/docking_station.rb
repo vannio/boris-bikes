13e386fed38d1ba6969a5f95b5bce56cd8b7d8bb
Van Le <singularity@vannio.co.uk>
2016-05-27 12:30:25 +0100

    Refactored broken vs unbroken bikes into seperate arrays

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,33 +2,39 @@ require_relative 'bike'
 
 class DockingStation
 
-  attr_reader :bikes
+  attr_reader :bikes, :bikes_broken
+
   attr_accessor :capacity
   DEFAULT_CAPACITY = 20
 
   def initialize(capacity = DEFAULT_CAPACITY)
     @bikes = []
+    @bikes_broken = []
     @capacity = capacity
   end
 
   def dock(bike)
     fail 'Error: Docking Station Full' if full?
-  	@bikes << bike
+  	if bike.working?
+      @bikes << bike
+    else
+      @bikes_broken << bike
+    end
   end
 
   def release_bike
     fail 'Error. No bikes available' if empty?
-    @bikes.pop if @bikes.last.working?
+    @bikes.pop
   end
 
   def release_broken_bikes
-    @bikes.pop unless @bikes.last.working?
+    @bikes_broken.pop
   end
 
   private
 
   def full?
-    @bikes.count >= capacity
+    @bikes.count + @bikes_broken.count >= @capacity
   end
 
   def empty?
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -20,7 +20,7 @@ describe DockingStation do
     it 'does not release a broken bike' do
       bike = double(:bike, :working? => false)
       subject.dock(bike)
-      expect(subject.release_bike).to_not eq bike
+      expect {subject.release_bike}.to raise_error 'Error. No bikes available'
     end
 
     it 'releases broken bikes to a van' do
@@ -36,8 +36,17 @@ describe DockingStation do
     end
 
     it 'raises error when station is full' do
-      DockingStation::DEFAULT_CAPACITY.times {subject.dock(double :bike)}
-      expect {subject.dock(double :bike)}.to raise_error 'Error: Docking Station Full'
+      bike = double(:bike, :working? => false)
+      DockingStation::DEFAULT_CAPACITY.times {subject.dock(bike)}
+      expect {subject.dock(bike)}.to raise_error 'Error: Docking Station Full'
+    end
+
+    it 'seperates broken and working bikes into different arrays' do
+      bike = double(:bike, :working? => true)
+      expect(subject.dock(bike)).to eq subject.bikes
+
+      bike_broken = double(:bike, :working? => false)
+      expect(subject.dock(bike_broken)).to eq subject.bikes_broken
     end
 
     describe 'DEFAULT_CAPACITY' do
