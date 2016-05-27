3117a54cead38e04ca603a1560c90755c306d540
Van Le <singularity@vannio.co.uk>
2016-05-27 09:48:09 +0100

    Updated broken bike rspec test

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,14 +3,12 @@
 require 'bike'
 
 describe Bike do
-	it "checks if bike is working" do
+	it 'checks if bike is working' do
 		expect(subject).to respond_to :working?
 	end
 
-	describe 'broken bike' do
-	  it 'is not working' do
-			subject.report_broken
-	    expect(subject).to be_broken
-	  end
-	end
+  it 'checks if a bike is broken' do
+		subject.report_broken
+    expect(subject.working?).to eq false
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,44 +1,45 @@
-require 'DockingStation'
+require 'docking_station'
 
 describe DockingStation do
 
-  it "responds to release_bike" do
-    expect(subject).to respond_to :release_bike
-  end
-
-  it "releases the working bike" do
-  	bike = Bike.new
-  	subject.dock(bike)
-	  bike = subject.release_bike
-    expect(bike).to be_working
-  end
-
-  it "docks the bike" do
-    expect(subject).to respond_to :dock
-  end
-
   describe '#release_bike' do
-  	it 'releases bike' do
-  	  bike = Bike.new
-  	  subject.dock(bike)
-  	  expect(subject.release_bike).to eq bike
+    it 'responds to release_bike' do
+      expect(subject).to respond_to :release_bike
+    end
+
+    it 'releases bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
     end
 
   	it 'raises error if no bikes available' do
   		expect {subject.release_bike}.to raise_error 'Error. No bikes available'
   	end
 
-    describe '#dock' do
-    	it 'raises error when station is full' do
-    		DockingStation::DEFAULT_CAPACITY.times {subject.dock(Bike.new)}
-    		expect {subject.dock(Bike.new)}.to raise_error 'Error: Docking Station Full'
-    	end
+    # it 'releases the working bike' do
+    #   bike = Bike.new
+    #   subject.dock(bike)
+    #   bike = subject.release_bike
+    #   expect(bike).to be_working
+    # end
+  end
 
-      describe 'DEFAULT_CAPACITY' do
-      	it 'gives a DEFAULT_CAPACITY of 20' do
-      		expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
-      	end
+  describe '#dock' do
+    it 'responds to docking the bike' do
+      expect(subject).to respond_to :dock
+    end
+
+    it 'raises error when station is full' do
+      DockingStation::DEFAULT_CAPACITY.times {subject.dock(Bike.new)}
+      expect {subject.dock(Bike.new)}.to raise_error 'Error: Docking Station Full'
+    end
+
+    describe 'DEFAULT_CAPACITY' do
+      it 'gives a DEFAULT_CAPACITY of 20' do
+        expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
       end
     end
   end
+
 end
