b35c6d017e9ca243d843d06ee20ff04b6c7fae1f
Van Le <singularity@vannio.co.uk>
2016-05-27 09:59:07 +0100

    Docking station doesn't release a broken bike. Hoorah

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -12,15 +12,13 @@ class DockingStation
   end
 
   def dock(bike)
-    	#docked? == true
     fail 'Error: Docking Station Full' if full?
   	@bikes << bike
   end
 
   def release_bike
-    	#Bike.new if empty_bikes == false
     fail 'Error. No bikes available' if empty?
-    @bikes.pop
+    @bikes.pop if @bikes.last.working?
   end
 
   private
@@ -33,17 +31,3 @@ class DockingStation
     @bikes.empty?
   end
 end
-
-
-
-# station = DockingStation.new
-# 10.times do bike = Bike.new
-# end
-
-# p station.full?
-
-
-
-  #def empty_bikes
-  # return 'Error. No bikes available'
-  #end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -17,12 +17,12 @@ describe DockingStation do
   		expect {subject.release_bike}.to raise_error 'Error. No bikes available'
   	end
 
-    # it 'releases the working bike' do
-    #   bike = Bike.new
-    #   subject.dock(bike)
-    #   bike = subject.release_bike
-    #   expect(bike).to be_working
-    # end
+    it 'releases the working bike' do
+      bike = Bike.new
+      bike.report_broken
+      subject.dock(bike)
+      expect(subject.release_bike).to_not eq bike 
+    end
   end
 
   describe '#dock' do
