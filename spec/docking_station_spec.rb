e5ca969e0c768b40065f2982cea9e231df5fc139
Van Le <singularity@vannio.co.uk>
2016-05-27 11:35:47 +0100

    Updated broken-bike-release description msg

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -17,11 +17,11 @@ describe DockingStation do
   		expect {subject.release_bike}.to raise_error 'Error. No bikes available'
   	end
 
-    it 'releases the working bike' do
+    it 'does not release a broken bike' do
       bike = Bike.new
       bike.report_broken
       subject.dock(bike)
-      expect(subject.release_bike).to_not eq bike 
+      expect(subject.release_bike).to_not eq bike
     end
   end
 
